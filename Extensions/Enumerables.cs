186efd8c088152b188f99890a1d8cad547d26b9e
Yonathan Probeh <Probeh@outlook.com>
2021-04-27 11:22:35 +0300

    Source Update

--- Extensions/Enumerables.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace SLA_Report.Extensions {
-  public static partial class Extensions {
-    public static int GetIndex<T>(this T value) where T : struct, Enum => (int) (IConvertible) value;
-    public static string GetName<T>(this T value) where T : struct, Enum => Enum.GetName<T>(value);
-  }
-}
